36e92edc9f4272638173949def754a311c0477f6
alxmart <alxmartinez@gmail.com>
2024-03-17 11:31:14 -0300

    Added more OOP tests / exercises.

--- app/src/main/java/com/luizafmartinez/at01_kotlin/lesson03/Permission_Generic_Class.kt
+++ app/src/main/java/com/luizafmartinez/at01_kotlin/lesson03/Permission_Generic_Class.kt
@@ -1,8 +1,7 @@
 package com.luizafmartinez.at01_kotlin.lesson03
 
 // Generic Class
-
-class Permission <T> {
+class Permission_Generic_Class <T> {
     var userName:T? = null
     var password:T? = null
     var iD:T? = null
--- app/src/main/java/com/luizafmartinez/at01_kotlin/lesson03/RunPermission.kt
+++ app/src/main/java/com/luizafmartinez/at01_kotlin/lesson03/RunPermission.kt
@@ -2,19 +2,16 @@ package com.luizafmartinez.at01_kotlin.lesson03
 
 fun main (){
 
-    val x = Permission<String> ()
-
+    val x = Permission_Generic_Class<String> ()
     x.userName = "lenny@androidcompany.com"
-    x.password = "Toronto@1234"
+    x.password = "Sampa@1234"
     println("User Name: ${x.userName}  & Password: ${x.password}")
 
-    val y = Permission<String> ()
-
+    val y = Permission_Generic_Class<String> ()
     y.userName = "homer@springfield.com"
     y.password = 123456.toString()
     println("User Name: ${y.userName}  & Password: ${y.password}")
 
-    val z = Permission<Int>()
-
-    z.iD = 2022
+    val z = Permission_Generic_Class<Int>()
+    z.iD = 1031
     println("User ID: ${z.iD} ")}
